912886c9755c5343f32977c7ae14bc235d8d0806
Justin <122790149+jquagga@users.noreply.github.com>
2025-04-21 17:23:08 -0400

    Update main.py
    Apparently the indicator field needs the zero to be a string. Ok then.

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def update_device():
             # And now lets go through and make sure the 3 indicators are turned off
             # They seem to turn on for no reason?
             for indicator in ["indicator1", "indicator2", "indicator3"]:
-                requests.post(f"http://{host}/api/{indicator}", json={"color": 0})
+                requests.post(f"http://{host}/api/{indicator}", json={"color": "0"})
             print(f"{host} updated!")
         except requests.exceptions.RequestException as e:
             print(e)
